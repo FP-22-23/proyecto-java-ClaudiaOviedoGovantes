684a602259263591bcf2f187c7a85fb4d0d859e8
ClaudiaOviedoGovantes <claudiaogvizcaino@gmail.com>
2023-03-05 10:44:01 +0100

    Implementación ToString, HashCode, CompareTo, FormatoCorto(p.derivada)

---
+++ src/fp/test/TestDataScience.java
@@ -0,0 +1,46 @@
+package fp.test;
+
+import java.time.LocalDate;
+
+import fp.utiles.DataScienceJob;
+
+public class TestDataScience {
+
+	public static void main(String[] args) {
+		
+		//Constructor 1
+		DataScienceJob d1 = new DataScienceJob("Zepter", 509, "Nizip", LocalDate.of(2006,10,15), 2.76, true, 
+				true); 
+		/*
+		System.out.println(d1);
+		System.out.println(d1.getCompany());
+		System.out.println(d1.getEmployee());
+		System.out.println(d1.getCity());
+		System.out.println(d1.getFoundation());
+		System.out.println(d1.getRating());
+		System.out.println(d1.getIsPrivate());
+		System.out.println(d1.getEasyApply());
+		System.out.println(d1.getFormatoCorto());
+		*/
+
+		//Constructor 2
+		DataScienceJob d2 = new DataScienceJob("Zepter,	509,	Nizip,	15/10/06,	2.76,	true,	"
+				+ "true,	Information_Technology,	(Work Under Pressure)");
+		/*
+		System.out.println(d2);
+		System.out.println(d2.getCompany());
+		System.out.println(d2.getEmployee());
+		System.out.println(d2.getCity());
+		System.out.println(d2.getFoundation());
+		System.out.println(d2.getRating());
+		System.out.println(d2.getIsPrivate());
+		System.out.println(d2.getFormatoCorto());
+		*/
+		
+		System.out.println(d1.compareTo(d2));
+		System.out.println(d1.equals(d2)); //???
+		//HACER COMPROBACIONES EQUALS, HASHCODE, TOSTRING
+		
+	}
+
+}
--- src/fp/utiles/DataScienceJob.java
+++ src/fp/utiles/DataScienceJob.java
@@ -2,6 +2,7 @@ package fp.utiles;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 import fp.common.Sector;
 
@@ -14,14 +15,14 @@ public class DataScienceJob {
 	private Double rating;
 	private Boolean isPrivate;
 	private Boolean easyApply;
-	private String sector;
+	private String sector; //hay que poner sector aqui?? 
 	private String skills;
+
 	
 	//Constructores
 		//1
 	public DataScienceJob(String company, Integer employee, String city,
-			LocalDate foundation, Double rating, Boolean isPrivate, Boolean easyApply, 
-			String sector, String skills) {
+			LocalDate foundation, Double rating, Boolean isPrivate, Boolean easyApply) {
 		this.company = company;
 		this.employee = employee;
 		this.city = city;
@@ -29,6 +30,8 @@ public class DataScienceJob {
 		this.rating = rating;
 		this.isPrivate = isPrivate;
 		this.easyApply = easyApply;
+		this.sector = null;
+		this.skills = null;
 		
 	}
 		//2
@@ -39,12 +42,14 @@ public class DataScienceJob {
 					"Cadena con formato no válido");
 		}
 		this.company = sp[0].trim();
-		this.employee = Integer.valueOf(sp[1].trim()); // Hay que convertir los tipos con value of.
+		this.employee = Integer.valueOf(sp[1].trim()); 
 		this.city = sp[2].trim();
 		this.foundation = LocalDate.parse(sp[3].trim(), DateTimeFormatter.ofPattern("dd/MM/yy"));
 		this.rating = Double.valueOf(sp[4].trim());
 		this.isPrivate = formateoBooleano(sp[5].trim());
 		this.easyApply = formateoBooleano(sp[6].trim());
+		this.sector = sp[7].trim();
+		this.skills = sp[8].trim();
 	}
 	
 	private Boolean formateoBooleano(String s){
@@ -55,6 +60,13 @@ public class DataScienceJob {
 		else {
 			res = false;
 		}
+		return res;
+	}
+	private LocalDate fechaFormateada(LocalDate d) {
+		LocalDate res;
+		String d2 = String.valueOf(d);
+		System.out.println();
+		res =  LocalDate.parse(d2, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		return res;
 	}
 
@@ -72,7 +84,7 @@ public class DataScienceJob {
 	}
 
 	public LocalDate getFoundation() {
-		return foundation;
+		return foundation; 
 	}
 
 	public Double getRating() {
@@ -90,21 +102,30 @@ public class DataScienceJob {
 	public String getSector() {
 		return sector;
 		
-		}
+	}
 	
 	public String getSkills() {
 	return skills;
 	
 	}
 	
-	public Sector getSectorEnum() {
-		String s = getSector();
+	public Sector getSectorEnum(String s) {
 		Sector res = null;
 		switch (s) {
-		case "TOURISM":
-			//res = Sector.TOURISIM;
+		case "Tourism":
+			res = Sector.TOURISM;
 			break;
-			
+		case "Information_Technology":
+			res = Sector.INFORMATION_TECHNOLOGY;
+			break;
+		case "Consumer_Services":
+			res = Sector.CONSUMER_SERVICES;
+			break;
+		case "Finance":
+			res = Sector.FINANCE;
+			break;
+		default:
+			res = Sector.MEDIA;
 			
 		}
 		return res;
@@ -145,12 +166,68 @@ public class DataScienceJob {
 	public void setSkills(String skills) {
 		this.skills = skills;
 	}
+	
+//____FormatoCorto___
+	public String getFormatoCorto() {
+		String res = getCompany();
+		if (getFoundation() != null) {
+			res += ", was founded in " + getFoundation().getYear();
+		}
+		if (getRating() != null) {
+			res += ", has a rating of " + getRating()+"/5";
+		}
+		return res;
+	}
+	
 
 	
-	//public Sector getSector() {
-	//	Sector res = null;
+//____ToString____
+	public String toString() {
+		String res =  "DataScienceJob [company=" + company + ", employee=" + employee + ", city=" + city + ", foundation="
+				+ foundation + ", rating=" + rating + ", isPrivate=" + isPrivate + ", easyApply=" + easyApply;
+		if (sector != null) {
+				res +=", sector=" + getSectorEnum(sector);
+			}
+		if (skills != null) {
+			res += ", skills=" + skills + "]";
+		}
+		return res;
+				
+	}
+//____HashCode____
+	public int hashCode() {
+		return Objects.hash(city, company, easyApply, employee, foundation, isPrivate, rating);
+	}
+
+//_____Equals____
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DataScienceJob other = (DataScienceJob) obj;
+		return Objects.equals(city, other.city) && Objects.equals(company, other.company)
+				&& Objects.equals(easyApply, other.easyApply) && Objects.equals(employee, other.employee)
+				&& Objects.equals(foundation, other.foundation) && Objects.equals(isPrivate, other.isPrivate)
+				&& Objects.equals(rating, other.rating);
+	}
+
+
+//___CompareTo
+	public int compareTo(DataScienceJob j1) {
+		int r = this.getCity().compareTo(j1.getCity());
+		if (r==0) {
+			r = this.getCompany().compareTo(j1.getCompany());
+		}
+		if (r==0) {
+			r = this.getFoundation().compareTo(j1.getFoundation());
+		}
 		
-	//}
+		return r;
+	}
+	
 
 	
 	
